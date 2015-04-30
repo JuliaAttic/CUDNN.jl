c2166838ed3bbf616418aa137869e8da668d191b
Deniz Yuret <denizyuret@gmail.com>
2015-04-30 16:25:27 +0300

    *** empty log message ***

--- src/CUDNN.jl
+++ src/CUDNN.jl
@@ -1,8 +1,17 @@
 module CUDNN
+using CUDArt
 
 const libcudnn = find_library(["libcudnn"])
 isempty(libcudnn) && error("CUDNN library cannot be found")
+
 include("libcudnn_types.jl")
 include("libcudnn.jl")
+include("tensor.jl")
+
+# setup cudnn handle
+cudnnhandle = cudnnHandle_t[0]
+cudnnCreate(cudnnhandle)
+# destroy cudnn handle at julia exit
+atexit(()->cudnnDestroy(cudnnhandle[1]))
 
 end # module
